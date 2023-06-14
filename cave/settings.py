be2ed51133f7340bece14d58cba192ff3f468461
Mukelabai01 <mukelabailucasmaboshe@gmail.com>
2023-06-14 11:59:48 -0700

    Initial commit

--- cave/settings.py
+++ cave/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-bohs7#v3-s1w2(6fh_l4%fc!9z265i!cg4s2wl3xmf5^l5_a_i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
